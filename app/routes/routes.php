2a1751e306a7a2027b6bcfbc4bfb100baee8280b
Medshot <chornogovlada@gmail.com>
2021-07-25 19:12:05 +0300

    create routes

---
+++ app/routes/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    // PageController
+    '/' => [
+        'controller' => 'PageController',
+        'action' => 'index'
+    ],
+
+    // ImportController
+    '/import' => [
+        'controller' => 'ImportController',
+        'action' => 'index'
+    ],
+
+    // FilmController
+    '/film/(\d+)' => [
+        'controller' => 'FilmController',
+        'action' => 'show'
+    ],
+    '/films' => [
+        'controller' => 'FilmController',
+        'action' => 'showAll'
+    ],
+    '/film/create' => [
+        'controller' => 'FilmController',
+        'action' => 'create'
+    ],
+    '/film/update/(\d+)' => [
+        'controller' => 'FilmController',
+        'action' => 'update'
+    ],
+    '/film/delete/(\d+)' => [
+        'controller' => 'FilmController',
+        'action' => 'delete'
+    ],
+];
