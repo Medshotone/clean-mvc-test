7a2570c6b619172265040f52bd1e4fb6ea9b05e8
Medshot <chornogovlada@gmail.com>
2021-07-26 00:42:25 +0300

    try traits

---
+++ app/traits/UrlTrait.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\traits;
+
+trait UrlTrait
+{
+    public function getIdFromUrl()
+    {
+        $exlodedUrl = explode('/', $_SERVER['REQUEST_URI']);
+
+        $id = end($exlodedUrl);
+
+        return $id;
+    }
+}
