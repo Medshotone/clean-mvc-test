a70d1ef796d283d7c5683dc657f7f608f0d70a01
Medshot <chornogovlada@gmail.com>
2021-07-26 14:57:57 +0300

    add sanitize from

--- app/controllers/FilmController.php
+++ app/controllers/FilmController.php
@@ -57,33 +57,64 @@ class FilmController extends Controller
             $this->view->returnJson(['wrongRequestMethod' => 'Wrong request method, must be POST'], 400);
         }
 
+        $this->sanitizeForm($_POST);
+
         if (!$this->validateForm($_POST)) {
             $this->view->returnJson(['errors' => $this->error]);
         }
 
-        if ($this->film->create($_POST)) {
-            $_SESSION['successMessage'] = 'Film successfully added!!!';
+        if (!$this->film->create($_POST)) {
+            $this->view->returnJson(['dbInsertFail' => 'Insert in database']);
+        }
 
-            $this->view->returnJson(['redirect' => '/films']);
+        $_SESSION['successMessage'] = 'Film successfully added!!!';
+
+        $this->view->returnJson(['redirect' => '/films']);
+    }
+
+    protected function sanitizeForm(array &$postData)
+    {
+        if (isset($postData['title'])) {
+            $postData['title'] = (string)htmlentities($postData['title']);
+        } else {
+            $postData['title'] = '';
+        }
+
+        if (isset($postData['release_year'])) {
+            $postData['release_year'] = (int)$postData['release_year'];
+        } else {
+            $postData['release_year'] = 0;
+        }
+
+        if (isset($postData['format'])) {
+            $postData['format'] = (string)htmlentities($postData['format']);
+        } else {
+            $postData['format'] = '';
+        }
+
+        if (isset($postData['stars'])) {
+            $postData['stars'] = (string)htmlentities($postData['stars']);
+        } else {
+            $postData['stars'] = '';
         }
     }
 
     protected function validateForm(array $postData)
     {
-        if (!isset($postData['title']) || (mb_strlen($postData['title']) < 1) || (mb_strlen($postData['title']) > 64)) {
+        if ((mb_strlen($postData['title']) < 1) || (mb_strlen($postData['title']) > 64)) {
             $this->error['title'] = 'Title must be between 1 and 64';
         }
 
-        if (!isset($postData['release_year']) || (int)$postData['release_year'] < 1895 || (int)$postData['release_year'] > (int)date('Y')) {
+        if ((int)$postData['release_year'] < 1895 || (int)$postData['release_year'] > (int)date('Y')) {
             $this->error['release_year'] = 'Release year must contain only year, and must be between 1895 and the current';
         }
 
-        if (!isset($postData['format']) || !$postData['format'] || !in_array((string)$postData['format'],
+        if (!$postData['format'] || !in_array((string)$postData['format'],
                 ['VHS', 'DVD', 'Blu-Ray'])) {
             $this->error['release_year'] = 'Release year must contain only year, and must be between 1895 and the current';
         }
 
-        if (!isset($postData['stars']) || mb_strlen($postData['stars']) < 1) {
+        if (mb_strlen($postData['stars']) < 1) {
             $this->error['stars'] = 'Please indicate name and surname actors';
         }
 
